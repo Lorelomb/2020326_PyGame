5f15acc32bbd0a3c8cbf852b31ed34a081226f18
Lorelomb <2020326@brunel.ac.uk>
2022-05-11 15:10:12 +0100

    Movement implementation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pygame.math import Vector2
 class DRAGON:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1, 0)
 
     # Create a rect and then draw the dragon's body
     def draw_dragon(self):
@@ -15,6 +16,11 @@ class DRAGON:
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
             pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+    def move_dragon(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 
 class MEAT:
@@ -37,12 +43,27 @@ clock = pygame.time.Clock()
 meat = MEAT()
 dragon = DRAGON()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+
 while True:
     # Draw all of the elements on the main display source
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            dragon.move_dragon()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                dragon.direction = Vector2(0, -1)
+            if event.key == pygame.K_RIGHT:
+                dragon.direction = Vector2(1, -0)
+            if event.key == pygame.K_LEFT:
+                dragon.direction = Vector2(-1, 0)
+            if event.key == pygame.K_DOWN:
+                dragon.direction = Vector2(0, 1)
 
     screen.fill((175, 215, 70))
     meat.draw_meat()
