40d66281c717c0c3a6cd3ee9620b31e973bebe1b
Lorelomb <2020326@brunel.ac.uk>
2022-05-11 13:33:18 +0100

    Meat implementation + additional changes to game window setup

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import pygame
+import sys
+import random
+from pygame.math import Vector2
+
+
+class MEAT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = pygame.math.Vector2(self.x, self.y)
+
+    def draw_meat(self):
+        meat_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        pygame.draw.rect(screen, (126, 166, 114), meat_rect)
+
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+
+meat = MEAT()
 
 while True:
     # draw all of the elements on the display source
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((175, 215, 70))
+    meat.draw_meat()
     pygame.display.update()
+    clock.tick(60)
