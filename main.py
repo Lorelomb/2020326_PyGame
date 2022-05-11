cf12b83ec9e457b9ce36349e5eba0d27b770c08e
Lorelomb <2020326@brunel.ac.uk>
2022-05-11 13:46:11 +0100

    Dragon implementation

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ import random
 from pygame.math import Vector2
 
 
+class DRAGON:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    # Create a rect and then draw the dragon's body
+    def draw_dragon(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+
 class MEAT:
     def __init__(self):
-        self.x = random.randint(0,cell_number - 1)
-        self.y = random.randint(0,cell_number - 1)
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
         self.pos = pygame.math.Vector2(self.x, self.y)
 
     def draw_meat(self):
@@ -22,9 +35,10 @@ screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_si
 clock = pygame.time.Clock()
 
 meat = MEAT()
+dragon = DRAGON()
 
 while True:
-    # draw all of the elements on the display source
+    # Draw all of the elements on the main display source
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -32,5 +46,6 @@ while True:
 
     screen.fill((175, 215, 70))
     meat.draw_meat()
+    dragon.draw_dragon()
     pygame.display.update()
     clock.tick(60)
