c6e308acd88fc901275304391e94df5bed4d1a1a
Lorelomb <2020326@brunel.ac.uk>
2022-05-16 03:53:31 +0100

    Dragon final assets + comments + Scoreboard changes

--- main.py
+++ main.py
@@ -7,18 +7,88 @@ from pygame.math import Vector2
 
 
 class DRAGON:
+    # Dragon spawn location on the grid
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
 
+        # Dragon body part animations
+        self.head_up = pygame.image.load('Assets/head up.png').convert_alpha()
+        self.head_down = pygame.image.load('Assets/head down.png').convert_alpha()
+        self.head_right = pygame.image.load('Assets/head right.png').convert_alpha()
+        self.head_left = pygame.image.load('Assets/head left.png').convert_alpha()
+
+        self.tail_up = pygame.image.load('Assets/tail up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Assets/tail down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Assets/tail right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Assets/tail left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Assets/body vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Assets/body horizontal.png').convert_alpha()
+
+        self.curved_tr = pygame.image.load('Assets/curved top right.png').convert_alpha()
+        self.curved_tl = pygame.image.load('Assets/curved top left.png').convert_alpha()
+        self.curved_br = pygame.image.load('Assets/curved bottom right.png').convert_alpha()
+        self.curved_bl = pygame.image.load('Assets/curved bottom left.png').convert_alpha()
+
     # Create a rect and then draw the dragon's body
     def draw_dragon(self):
-        for block in self.body:
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        # Block version for tests
+        # for block in self.body:
+        #     x_pos = int(block.x * cell_size)
+        #     y_pos = int(block.y * cell_size)
+        #     block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+        #     pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+        for index, block in enumerate(self.body):
+            # Rect for the positioning
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+            # The direction the face is heading to follow up with the correct animation
+            if index == 0:
+                screen.blit(self.head, block_rect)
+            # Check for the last item present in self.body
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                # Check if the X and Y value is the same if so vertical or horizontal body animation is played
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                # Check for the X and Y pos for the previous and next block in both directions
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.curved_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.curved_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.curved_tr, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.curved_br, block_rect)
+
+    # Check the first element in self.body and subtract it for the item that comes after, vector - vector = final vector
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1, 0): self.head = self.head_left
+        elif head_relation == Vector2(- 1, 0): self.head = self.head_right
+        elif head_relation == Vector2(0, 1): self.head = self.head_up
+        elif head_relation == Vector2(0, -1): self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0): self.tail = self.tail_right
+        elif tail_relation == Vector2(- 1, 0): self.tail = self.tail_left
+        elif tail_relation == Vector2(0, 1): self.tail = self.tail_down
+        elif tail_relation == Vector2(0, -1): self.tail = self.tail_up
 
     def move_dragon(self):
         if self.new_block == True:
@@ -46,7 +116,8 @@ class MEAT:
     def draw_meat(self):
         meat_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
         screen.blit(meat, meat_rect)
-        pygame.draw.rect(screen, (126, 166, 114), meat_rect)
+        # Rectangle demonstrating the food, for test purposes
+        # pygame.draw.rect(screen, (126, 166, 114), meat_rect)
 
     def randomize(self):
         self.x = random.randint(0, cell_number - 1)
@@ -98,8 +169,8 @@ class MAIN:
     def draw_score(self):
         score_text = str(len(self.dragon.body) - 3)
         score_surface = game_font.render(score_text, True, (56, 74, 12))
-        score_x = int(cell_size * cell_number - 1080)
-        score_y = int(cell_size * cell_number - 1150)
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
         score_rect = score_surface.get_rect(center=(score_x, score_y))
         meat_rect = meat.get_rect(midright=(score_rect.left, score_rect.centery))
         bg_rect = pygame.Rect(meat_rect.left, meat_rect.top, meat_rect.width + score_rect.width + 6, meat_rect.height)
@@ -107,24 +178,29 @@ class MAIN:
         pygame.draw.rect(screen, (167, 209, 61), bg_rect)
         screen.blit(score_surface, score_rect)
         screen.blit(meat, meat_rect)
-        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 3)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
 
 pygame.init()
+# Grid size
 cell_size = 40
-cell_number = 30
+cell_number = 20
+# Resize the grid to fit the screen when on fullscreen
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size), pygame.RESIZABLE)
 clock = pygame.time.Clock()
+# Load meat image
 meat = pygame.image.load('Assets/turkey.png').convert_alpha()
 monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
+# Load font for the scoreboard
 game_font = pygame.font.Font('Font/Retro_Future_Curly_E_Spaced.ttf', 25)
 
 fullscreen = False
 main_game = MAIN()
 
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 60)
+pygame.time.set_timer(SCREEN_UPDATE, 70)
 
+# Game loop
 while True:
     # Draw all of the elements on the main display source
     for event in pygame.event.get():
@@ -137,21 +213,26 @@ while True:
             if not fullscreen:
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
         if event.type == pygame.KEYDOWN:
+            # F for fullscreen mode
             if event.key == pygame.K_f:
                 fullscreen = not fullscreen
                 if fullscreen:
                     screen = pygame.display.set_mode((screen.get_width(), screen.get_height()), pygame.FULLSCREEN)
                 else:
                     screen = pygame.display.set_mode((screen.get_width(), screen.get_height()), pygame.RESIZABLE)
+            # Move up
             if event.key == pygame.K_UP:
                 if main_game.dragon.direction.y != 1:
                     main_game.dragon.direction = Vector2(0, -1)
+            # Move to right
             if event.key == pygame.K_RIGHT:
                 if main_game.dragon.direction.x != -1:
                     main_game.dragon.direction = Vector2(1, -0)
+            # Move to left
             if event.key == pygame.K_LEFT:
                 if main_game.dragon.direction.x != 1:
                     main_game.dragon.direction = Vector2(-1, 0)
+            # Move down
             if event.key == pygame.K_DOWN:
                 if main_game.dragon.direction.y != -1:
                     main_game.dragon.direction = Vector2(0, 1)
